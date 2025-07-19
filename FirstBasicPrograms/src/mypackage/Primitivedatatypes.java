760de6433a9583bbf40c1e38aa28e10e64fa0c06
srinikareddy13 <2420030617@klh.edu.in>
2025-07-19 11:35:04 +0530

    Add files via upload

---
+++ FirstBasicPrograms/src/mypackage/Primitivedatatypes.java
@@ -0,0 +1,26 @@
+package mypackage;
+
+public class Primitivedatatypes {
+public static void main(String[]args) {
+	byte b = 10;
+	short s =100;
+	int i = 1000;
+	long l =10000L;
+	
+	float f = 10.5f;
+	double d = 20.99;
+	
+	char c ='A';
+			
+			boolean bool = true;
+	
+	System.out.println("byte value:" +b);
+	System.out.println("short value:" +s);
+	System.out.println("int value:" +i);
+	System.out.println("long value:" +l);
+	System.out.println("float value:" +f);
+	System.out.println("double value:" +d);
+	System.out.println("char value:" +c);
+	System.out.println("boolean value:"+bool);
+}
+}
---
+++ FirstBasicPrograms/src/mypackage/first.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
